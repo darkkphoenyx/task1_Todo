6d1a1cb61bee2f37d68cd99337de07f56c7e1f55
deepgeneral33@gmail.com <deepgeneral33@gmail.com>
2024-12-29 20:11:54 +0545

    Landing page designing initiated

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,9 @@
 export default function HomePage() {
   return (
     <>
-      <div>
+      <div className="container mx-auto flex justify-center items-center flex-col space-y-4">
         <h1 className="header">Welcome to a simple todo app</h1>
+        <h3>ToDo List</h3>
       </div>
     </>
   );
