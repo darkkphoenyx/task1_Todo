b9738f94a170e3c35d63bf3422423ee7278241ed
deepgeneral33@gmail.com <deepgeneral33@gmail.com>
2024-12-29 20:27:27 +0545

    material UI added to frontend

--- frontend/src/components/SearchBox.tsx
+++ frontend/src/components/SearchBox.tsx
@@ -1,3 +1,13 @@
 export const SearchBox = () => {
-  return <div className="search-box"></div>;
+  return (
+    <div className="search-box">
+      <div>
+        <input
+          className="border p-1 rounded-md outline-none border-gray-500"
+          type="text"
+          placeholder="Search note..."
+        />
+      </div>
+    </div>
+  );
 };
