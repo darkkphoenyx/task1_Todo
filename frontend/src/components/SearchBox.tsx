c17ffef7012f16755652b62731c1497877c6a28e
deepgeneral33@gmail.com <deepgeneral33@gmail.com>
2024-12-31 22:47:33 +0545

    design chages

--- frontend/src/components/SearchBox.tsx
+++ frontend/src/components/SearchBox.tsx
@@ -1,7 +1,7 @@
 export const SearchBox = () => {
   return (
     <div className="search-box flex gap-2 w-full justify-center">
-      <div className="flex items-center border border-[--primary-color] p-1 rounded-md border-gray-500">
+      <div className="flex items-center border border-[--primary-color] p-1 rounded-md">
         <input
           className="flex-grow p-1 outline-none"
           type="text"
@@ -9,7 +9,7 @@ export const SearchBox = () => {
         />
         <img src="/assets/search.svg" alt="search icon" />
       </div>
-      <div className="filter ">
+      <div className="filter">
         <select
           className="outline-none p-2 rounded-md bg-[--primary-color] text-white"
           name="filter"
@@ -18,6 +18,11 @@ export const SearchBox = () => {
           <option value="2">Completed</option>
           <option value="3">Remaining</option>
         </select>
+      </div>
+      <div>
+        <button className="p-2 bg-[--secondary-color] rounded-md active:translate-y-1 active:bg-[--primary-color]  transition-all">
+          Search
+        </button>
       </div>
     </div>
   );
